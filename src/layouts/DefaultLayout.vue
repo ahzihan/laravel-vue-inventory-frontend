050e563254d4b78063e36eb366a090c2a23ff723
Md Akbar Hossain <akbarhossain160@gmail.com>
2024-09-11 10:06:43 +0600

    primary setup and all asset updated

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,37 @@
+<template>
+   <div id="layout-wrapper">
+      <NavbarView />
+      <div class="hori-overlay"></div>
+      <div class="main-content">
+           <!-- Content Here -->
+           <router-view v-slot="{ Component, route }">
+              <transition name="route" mode="out-in">
+                  <div :key="route.name">
+                      <component :is="Component" />
+                  </div>
+              </transition>
+          </router-view>
+      </div>
+   </div>
+</template>
+
+<script setup>
+import NavbarView from '@/components/Navbar.vue'
+</script>
+
+<style>
+.route-enter-from{
+  opacity: 0;
+  transform:  translateY(10px);
+}
+
+.route-enter-active,
+.route-leave-active{
+  transition: all 0.2s ease-out;
+}
+
+.route-leave-to{
+  opacity: 0;
+  transform:  translateY(10px);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,32 @@
+import AuthLayout from '@/layouts/AuthLayout.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/dashboard',
+      component: DefaultLayout,
+      meta:{requiresAuth: true},
+      children: [
+        {path: '/dashboard', name: 'dashboard',component: () => import('@/views/Dashboard.vue')}
+      ],
     },
+
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/auth',
+      redirect: '/login',
+      component: AuthLayout,
+      meta:{isGuest: true},
+      children: [
+        {path: '/login', name: 'login',component: () => import('@/views/Auth/Login.vue')}
+      ],
     }
+    
   ]
 })
 
