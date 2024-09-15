b500bd5dcb81d9f027bed33bc859c72ca6640e3c
Md Akbar Hossain <akbarhossain160@gmail.com>
2024-09-15 09:13:32 +0600

    sweet alert intrigrate done

--- src/main.js
+++ src/main.js
@@ -6,11 +6,17 @@ import App from './App.vue'
 import router from './router';
 
 import VeeValidatePlugin from './utils/validation';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(VeeValidatePlugin)
+app.use(VueSweetalert2, {
+    confirmButtonColor: '#A97EEA',
+    cancelButtonColor: '#FF7674',
+})
 
 app.mount('#app')
